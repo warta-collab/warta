9ba91f25a71c3aed1ab71ed5c97455b25bff0284
Mochamad Afif Risdiyanto <53353746+afdi18@users.noreply.github.com>
2022-12-02 11:28:39 +0700

    Update app_modules.dart

--- lib/app/modules/app_modules.dart
+++ lib/app/modules/app_modules.dart
@@ -4,10 +4,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:shimmer/shimmer.dart';
 
 // ------------- Package App --------------
 import 'package:warta/app/data/app_data.dart';
 import 'package:warta/app/routes/app_pages.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 // ------------- Modules ----------------
 // Splash module
@@ -18,8 +21,12 @@ part 'splash/controllers/splash_controller.dart';
 // Home module
 part 'home/views/home_view.dart';
 part 'home/views/item_view.dart';
+part 'home/views/item_view_shimmer.dart';
+part 'home/views/list_kategori.dart';
+part 'home/views/search_bar.dart';
 part 'home/bindings/home_binding.dart';
 part 'home/controllers/home_controller.dart';
+
 
 // Detail module
 part 'detail/views/detail_view.dart';
