8f37ea8066aa2e803b0084042f26431aa86e4478
Mochamad Afif Risdiyanto <53353746+afdi18@users.noreply.github.com>
2022-12-02 11:21:51 +0700

    Update home_controller.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,12 +2,41 @@ part of '../../app_modules.dart';
 
 class HomeController extends GetxController {
   NewsProvider newsProvider = NewsProvider();
+  late TabController tabController;
   var news = List<News>.empty().obs;
+  var searchNews = List<News>.empty().obs;
+  var ktgr = "".obs;
+  var negara = "id".obs;
+  List<Tab> tabKtgr = <Tab>[
+    Tab(
+      text: 'General',
+    ),
+    Tab(
+      text: 'Business',
+    ),
+    Tab(
+      text: 'Science',
+    ),
+    Tab(
+      text: 'Technology',
+    ),
+    Tab(
+      text: 'Health',
+    ),
+    Tab(
+      text: 'Sports',
+    ),
+    Tab(
+      text: 'Entertainment',
+    ),
+  ];
 
   @override
   void onInit() {
-    loadNews();
     super.onInit();
+    tabController = TabController(length: tabKtgr.length, vsync: this);
+    tabController.addListener(_onTabChanged);
+    loadNews();
   }
 
   @override
@@ -15,21 +44,53 @@ class HomeController extends GetxController {
     super.onReady();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    tabController.dispose();
+  }
+
+  void _onTabChanged() {
+    if (!tabController.indexIsChanging) {
+      news.clear();
+      // loadSearchNews("tek");
+      loadNews();
+      print("indeks berubah $ktgr");
+    }
+  }
+
+  Future<void> _onRefresh() async {
+    news.clear();
+    await loadNews();
+  }
+
+  void logout() {
+    _googleSignIn.disconnect().then((value) {
+      if (currentUser == null) Get.offAllNamed(Routes.LOGIN);
+    });
+  }
 
   Future<void> loadNews() async {
-    Response response = await newsProvider.getTopNews();
+    Response response = await newsProvider.getTopNews(negara, ktgr);
 
-    // print(response.statusCode);
     if (response.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(jsonEncode(response.body));
       List<dynamic> body = json['articles'];
       List<News> articles =
           body.map((dynamic item) => News.fromJson(item)).toList();
       news.addAll(articles);
+    }
+  }
+
+  Future<void> loadSearchNews(String qword) async {
+    Response response = await newsProvider.getNewsByQword(qword);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(jsonEncode(response.body));
+      List<dynamic> body = json['articles'];
+      List<News> articles =
+          body.map((dynamic item) => News.fromJson(item)).toList();
+      searchNews.addAll(articles);
     }
   }
 }
